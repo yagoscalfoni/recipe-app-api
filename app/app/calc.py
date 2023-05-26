da4fa4d0c406be3d9f52bdc0090db92f11bd780a
Yago Escalfoni <yago@otimagestao.com>
2023-05-26 19:53:14 -0300

    commit

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """
+    Addd x and y and return result.
+    """
+    return x + y
+
+def subtract(x, y):
+    """Subtract x and y and return result"""
+    return y - x
