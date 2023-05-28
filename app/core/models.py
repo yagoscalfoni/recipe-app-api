a71da9b6cd3fff5adf77e9e1896f890089ae7936
Yago Escalfoni <yago@otimagestao.com>
2023-05-28 12:15:43 -0300

    Fixing Migrations at Models

--- app/core/models.py
+++ app/core/models.py
@@ -24,11 +24,10 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
     email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(max_lenght=255)
+    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
-    
+
     USERNAME_FIELD = 'email'
-    
